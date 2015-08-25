0ccc671e5a5a9a19d1ce8e29b6c35141f1df0f4f
zhaoyan <zhaoyan@zhaoyans-Mac-mini.local>
2015-08-25 15:11:30 +0800

    Initial Commit

---
+++ 核心动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  核心动画
+//
+//  Created by zhaoyan on 8/25/15.
+//  Copyright (c) 2015 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 核心动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  核心动画
+//
+//  Created by zhaoyan on 8/25/15.
+//  Copyright (c) 2015 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
